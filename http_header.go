0148dd52cfa9957b56ca57d5ab53f2d16de405b2
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-04-30 23:48:06 +0200

    replace kinds with their definition

--- http_header.go
+++ http_header.go
@@ -56,13 +56,13 @@ func HTTPHeaderUnmarshal(dec *jsontext.Decoder, h *http.Header) error {
 		return fmt.Errorf("expected begin object, got %s", tkn.Kind())
 	}
 
-	for dec.PeekKind() != '}' {
+	for dec.PeekKind() != jsontext.KindEndObject {
 		keyTkn, err := dec.ReadToken()
 		if err != nil {
 			return err
 		}
 
-		if keyTkn.Kind() != '"' {
+		if keyTkn.Kind() != jsontext.KindString {
 			return fmt.Errorf("expected string key, got %s", keyTkn.Kind())
 		}
 
@@ -73,13 +73,13 @@ func HTTPHeaderUnmarshal(dec *jsontext.Decoder, h *http.Header) error {
 			return err
 		}
 
-		if val.Kind() != '"' {
+		if val.Kind() != jsontext.KindString {
 			return fmt.Errorf("expected string value, got %s", val.Kind())
 		}
 
 		h.Set(key, val.String())
 	}
 
-	_, err = dec.ReadToken() // consume '}'
+	_, err = dec.ReadToken() // consume jsontext.KindEndObject
 	return err
 }
